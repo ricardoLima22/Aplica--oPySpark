568e8620d725e496e538542a8cdfd6694241ceaf
Ricardo Lima <ricardolimasouza27@gmail.com>
2024-10-26 14:29:49 -0300

    Selecionando as colunas email,cpf,estado,cor_favorita. E pegando apenas as pessoas que são do Estado ES, e com cor favorita ROXO

--- transform.py
+++ transform.py
@@ -3,8 +3,10 @@ import pyspark.sql.functions as F
 def teste_transform(df):
 
     logins = (
-       df.select(
+       df
+       .select(
        F.col("email"), F.col("cpf"),F.col("estado"),F.col("cor_favorita")
-       ).where(F.col("cor_favorita") == "Roxo")
+       ).where((F.col("cor_favorita") == "Roxo") & (F.col("estado") == "ES" ))
+       .where(F.col("email").like("%@gmail.com"))
     )
     return logins
